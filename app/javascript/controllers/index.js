ff30dfbc21656cf180faaad0630f62fbcfc2f751
Pusher <spunkspunkik@gmail.com>
2024-10-17 21:25:03 +0300

    added rooms, messages, views

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -1,4 +1,4 @@
 // Import and register all your controllers from the importmap via controllers/**/*_controller
 import { application } from "controllers/application"
-import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading"
-eagerLoadControllersFrom("controllers", application)
+import ChatRefreshController from "./chat_refresh_controller"
+application.register("chat_refresh", ChatRefreshController)
